d05b314823c64436cf03836635521e67f76b9271
Enrique Mendez <eqm@mit.edu>
2023-05-29 14:06:34 -0400

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trainkit',
+    version='0.0.1',
+    author='Enrique Mendez',
+    author_email='enrique.phys@email.com',
+    description='A library for simplifying training in Python',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
